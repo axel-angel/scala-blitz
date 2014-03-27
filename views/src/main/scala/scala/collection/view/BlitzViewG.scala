2e8ceba6809e69071f5e4bade30165301fe6366d
Axel Angel <axel+git@vneko.ch>
2014-03-27 18:38:15 +0100

    Add skel classes for BlitzView{CC,G} (two inputs or generator)

---
+++ views/src/main/scala/scala/collection/view/BlitzViewG.scala
@@ -0,0 +1,22 @@
+package scala.collection.views
+import ViewTransforms._
+
+import scala.collection.par._
+import workstealing.ResultCell
+
+
+/** BlitzView implementation with a generating function as source. */
+abstract class BlitzViewG[B] extends BlitzView[B] { self =>
+  val g: Int => A // generating function as source
+
+  /* TODO: this files contains only signature yet */
+  def >>[C](next: ViewTransform[B, C]): BlitzViewG[C] = ???
+
+  def map[C](f: B => C): BlitzViewG[C] = ???
+  def filter(p: B => Boolean): BlitzViewG[B] = ???
+  def reduce[R](z: => R)(op: (B, R) => R)(reducer: (R, R) => R)(implicit ctx: Scheduler): R = ???
+  def count()(implicit ctx: Scheduler): Int = ???
+  def min()(implicit ord: Ordering[B], ctx: Scheduler): Option[B] = ???
+  def max()(implicit ord: Ordering[B], ctx: Scheduler): Option[B] = ???
+}
+
