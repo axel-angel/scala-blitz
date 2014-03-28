e66cf7bb2e67689d6bb89e137471b2e3f2c0180b
Axel Angel <axel+git@vneko.ch>
2014-03-28 17:27:56 +0100

    Implement View.of

--- views/src/main/scala/scala/collection/view/BlitzViewsImpl.scala
+++ views/src/main/scala/scala/collection/view/BlitzViewsImpl.scala
@@ -3,6 +3,7 @@ package scala.collection.views
 import scala.collection.par._
 import workstealing.ResultCell
 import scala.collection.par.generic.IsReducable
+import scala.reflect.ClassTag
 
 
 object ViewTransforms {
@@ -35,13 +36,22 @@ object View {
     def transform = new ViewTransforms.Identity()
   }
 
+  /* // FIXME: not implemented yet
   def apply[T](gfunc: Int => T): BlitzView[T] = new BlitzViewG[T] {
     type A = T
     val g = gfunc
     def transform = new ViewTransforms.Identity()
   }
+  */
+
   def range(x: Int, y: Int)(implicit ctx: Scheduler): BlitzView[Int] = {
     val xs = x until (y+1)
     apply(xs.toPar)(rangeIsZippable(ctx))
+  }
+
+  def of[T](xss: T*)(implicit conv: IsReducable[Array[T], T], c: ClassTag[T]): BlitzView[T] = new BlitzViewC[T] {
+    type A = T
+    val xs = conv(xss.toArray.toPar)
+    def transform = new ViewTransforms.Identity()
   }
 }
