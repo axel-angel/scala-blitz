d2bf421609491e41ad61f276e2218e8b333a536c
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2014-03-28 16:15:43 +0100

    changes during discussion

--- views/src/test/scala/test.scala
+++ views/src/test/scala/test.scala
@@ -1,12 +1,15 @@
 import scala.collection.par._
-import scala.collection.views.BlitzViews._
+import scala.collection.views._
+import scala.collection.views.ViewTransforms._
 import scala.collection.par.Scheduler.Implicits.sequential
 
-val mymap = new Map[Int,Int](_ + 10)
-val myfilt = new Filter[Int](_ % 2 == 0)
-val mymap2 = new Map[Int,Double](_ * 1.0)
-val mystack = mymap >> myfilt >> mymap2
+object A{
+//val mymap = new Map[Int,Int](_ + 10)
+  val myfilt = new Filter[Int](_ % 2 == 0)
+//val mymap2 = new Map[Int,Double](_ * 1.0)
+  val mystack = myfilt >> myfilt
 
-val xs = 0 until 11
-val v = View(par2zippable(xs.toPar)) >> mystack
-v.count()
+  val xs = 0 until 11
+  val v = View(xs.toPar) map mystack
+  assert(v.size(x => true) == 6)
+}
