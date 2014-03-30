0a62de9a33d9e91baf1d21b40e3caa9722e369ed
Axel Angel <axel+git@vneko.ch>
2014-03-30 19:39:38 +0200

    Factorize basic tests for all views

--- views/src/test/scala/test.scala
+++ views/src/test/scala/test.scala
@@ -5,15 +5,22 @@ import scala.collection.par.Scheduler.Implicits.sequential
 
 object A {
   def main(args: Array[String]) {
-    testCount()
+    testCount(View((0 to 10).toPar))
+    testCount(View((0 until 11).toPar))
+    testCount(View(View((0 to 5).toPar), View((5 to 10).toPar)))
   }
 
-  def testCount() {
-    val xs = 0 until 11
-    val v = View(xs.toPar) map{_ + 10} filter{_ % 2 == 0} map{_ * 1.0}
+  def testCount(v1: BlitzView[Int]) {
+    val v = v1 map{_ + 10} filter{_ % 2 == 0} map{_ * 1.0}
     assert(v.size() == 6)
 
-    val sum = v aggregate(0.0){_ + _}{_ + _}
-    assert(sum == 90.0)
+    assert(v.aggregate(0.0){_ + _}{_ + _} == 90.0)
+    assert(v.reduce{_ + _} == 90.0)
+    assert(v.min == 10.0)
+    assert(v.max == 20.0)
+
+    View((0 until 0).toPar).reduce(_ + _) // FIXME: this should fail!
+
+    println("tests passed")
   }
 }
