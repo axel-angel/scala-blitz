43a47f65b54ea5ff421fb45c526ee6508034a009
Axel Angel <axel+git@vneko.ch>
2014-03-30 19:39:25 +0200

    Implement operations for BlitzViewVV

--- views/src/main/scala/scala/collection/view/BlitzViewVV.scala
+++ views/src/main/scala/scala/collection/view/BlitzViewVV.scala
@@ -11,17 +11,31 @@ abstract class BlitzViewVV[B] extends BlitzView[B] { self =>
   val ys: BlitzView[B] // 2nd source view
 
   override def >>[C](next: ViewTransform[B, C]) = new BlitzViewVV[C] {
-    val xs = self.xs map next
-    val ys = self.ys map next
+    val xs = self.xs >> next
+    val ys = self.ys >> next
   }
 
-  override def map[C](next: ViewTransform[B, C]): BlitzView[C] = ???
-  override def map[C](f: B => C): BlitzViewVV[C] = ???
-  override def filter(p: B => Boolean): BlitzViewVV[B] = ???
-  override def reduce(op: (B, B) => B)(implicit ctx: Scheduler): B = ???
-  override def aggregate[R](z: => R)(op: (B, R) => R)(reducer: (R, R) => R)(implicit ctx: Scheduler): R = ???
-  override def size()(implicit ctx: Scheduler): Int = ???
-  override def min()(implicit ord: Ordering[B], ctx: Scheduler): B = ???
-  override def max()(implicit ord: Ordering[B], ctx: Scheduler): B = ???
+  override def map[C](next: ViewTransform[B, C]): BlitzView[C] = >> [C](next)
+  override def map[C](f: B => C): BlitzViewVV[C] = self >> new Map[B,C](f)
+  override def filter(p: B => Boolean): BlitzViewVV[B] = self >> new Filter[B](p)
+
+  override def reduce(op: (B, B) => B)(implicit ctx: Scheduler): B = {
+    val x = xs.reduce(op)(ctx)
+    val y = ys.reduce(op)(ctx)
+    op(x, y)
+  }
+
+  override def aggregate[R](z: => R)(op: (B, R) => R)(reducer: (R, R) => R)(implicit ctx: Scheduler): R = {
+    val x = xs.aggregate(z)(op)(reducer)(ctx)
+    val y = ys.aggregate(z)(op)(reducer)(ctx)
+    reducer(x, y)
+  }
+
+  override def size()(implicit ctx: Scheduler): Int = xs.size() + ys.size()
+
+  override def min()(implicit ord: Ordering[B], ctx: Scheduler): B =
+    ord.min(xs.min(), ys.min())
+  override def max()(implicit ord: Ordering[B], ctx: Scheduler): B =
+    ord.max(xs.max(), ys.max())
 }
 
