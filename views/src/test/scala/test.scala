b02a549ac20e213b3307b3e50704e38d2999c7f3
Axel Angel <axel+git@vneko.ch>
2014-03-30 17:10:48 +0200

    Basic test in object A
    TODO: need integration with scalatest

--- views/src/test/scala/test.scala
+++ views/src/test/scala/test.scala
@@ -1,12 +1,19 @@
 import scala.collection.par._
-import scala.collection.views.BlitzViews._
+import scala.collection.views._
+import scala.collection.views.ViewTransforms._
 import scala.collection.par.Scheduler.Implicits.sequential
 
-val mymap = new Map[Int,Int](_ + 10)
-val myfilt = new Filter[Int](_ % 2 == 0)
-val mymap2 = new Map[Int,Double](_ * 1.0)
-val mystack = mymap >> myfilt >> mymap2
+object A {
+  def main(args: Array[String]) {
+    testCount()
+  }
 
-val xs = 0 until 11
-val v = View(par2zippable(xs.toPar)) >> mystack
-v.count()
+  def testCount() {
+    val xs = 0 until 11
+    val v = View(xs.toPar) map{_ + 10} filter{_ % 2 == 0} map{_ * 1.0}
+    assert(v.size() == 6)
+
+    val sum = v aggregate(0.0){_ + _}{_ + _}
+    assert(sum == 90.0)
+  }
+}
