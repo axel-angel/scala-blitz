cfb6a8de2b1264aff1df285c4cbb5b1a58c2363e
Axel Angel <axel+git@vneko.ch>
2014-05-04 18:45:57 +0200

    Rewrite tests to verify consistency with List operations

--- views/src/test/scala/test.scala
+++ views/src/test/scala/test.scala
@@ -5,11 +5,12 @@ import scala.collection.par.Scheduler.Implicits.sequential
 
 object A {
   def main(args: Array[String]) {
-    testCount(View((0 to 10).toPar))
-    testCount(View((0 until 11).toPar))
-    testCount(View(View((0 to 5).toPar), View((5 to 10).toPar)))
-    testCount(recView(0 to 10))
-    testCount(View(
+    val xs = (0 to 10).toList
+    test1(xs, View((0 to 10).toPar))
+    test1(xs, View((0 until 11).toPar))
+    test1(xs, View(View((0 to 5).toPar), View((5 to 10).toPar)))
+    test1(xs, recView(0 to 10))
+    test1(xs, View(
       View(
         View(vrange(0,1),vrange(2,3)),
         View(vrange(4,5),vrange(5,6))
@@ -32,26 +33,37 @@ object A {
     }
   }
 
-  def testCount(v1: BlitzView[Int]) {
+  // Compare BlitzView and List transformations, should stay consistent
+  def test1(l1: List[Int], v1: BlitzView[Int]) {
     val v = v1 map{_ + 10} filter{_ % 2 == 0} map{_ * 1.0}
-    assert(v.size() == 6)
+    val l = l1 map{_ + 10} filter{_ % 2 == 0} map{_ * 1.0}
+    testFolders(l, v)
+  }
 
-    assert(v.count{_ => true} == 6)
-    assert(v.count{_ % 4 == 0} == 3)
+  // Compare the results with different folding methods
+  def testFolders(l: List[Double], v: BlitzView[Double]) {
+    assert(v.size() == l.size)
 
-    assert(v.aggregate(0.0){_ + _}{_ + _} == 90.0)
-    assert(v.reduce{_ + _} == 90.0)
-    assert(v.min == 10.0)
-    assert(v.max == 20.0)
+    assert(v.count{_ => true} == l.count{_ => true})
+    assert(v.count{_ % 4 == 0} == l.count{_ % 4 == 0})
 
-    assert(v.find{_ == 20.0} == Some(20.0))
-    assert(v.find{_ == 21.0} == None)
+    assert(v.aggregate(0.0){_ + _}{_ + _} == l.aggregate(0.0)(_ + _, _ + _))
+    assert(v.reduce{_ + _} == l.reduce{_ + _})
+    assert(v.min == l.min)
+    assert(v.max == l.max)
 
-    assert(v.exists{_ >  20.0} == false)
-    assert(v.exists{_ >= 20.0} == true)
+    assert(v.find{_ == 20.0} == l.find{_ == 20.0})
+    assert(v.find{_ == 21.0} == l.find{_ == 21.0})
 
-    assert(v.forall{_ != 0.0} == true)
-    assert(v.forall{x => x >= 10 && x <= 20} == true)
+    assert(v.exists{_ >  20.0} == l.exists{_ >  20.0})
+    assert(v.exists{_ >= 20.0} == l.exists{_ >= 20.0})
+
+    assert(v.forall{_ != 0.0} == l.forall{_ != 0.0})
+    assert(v.forall{x => x >= 10 && x <= 20}
+        == l.forall{x => x >= 10 && x <= 20})
+
+    assert(v.sum == l.sum)
+    assert(v.product == l.product)
 
     println("tests passed")
   }
