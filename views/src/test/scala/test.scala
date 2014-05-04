4856522f946a9faee752e9344fc987472154b81d
Axel Angel <axel+git@vneko.ch>
2014-05-04 19:22:33 +0200

    Restructure tests, add more tests, add TODO

--- views/src/test/scala/test.scala
+++ views/src/test/scala/test.scala
@@ -6,11 +6,11 @@ import scala.collection.par.Scheduler.Implicits.sequential
 object A {
   def main(args: Array[String]) {
     val xs = (0 to 10).toList
-    test1(xs, View((0 to 10).toPar))
-    test1(xs, View((0 until 11).toPar))
-    test1(xs, View(View((0 to 5).toPar), View((6 to 10).toPar)))
-    test1(xs, recView(0 to 10))
-    test1(xs, View(
+    testAll(xs, View((0 to 10).toPar))
+    testAll(xs, View((0 until 11).toPar))
+    testAll(xs, View(View((0 to 5).toPar), View((6 to 10).toPar)))
+    testAll(xs, recView(0 to 10))
+    testAll(xs, View(
       View(
         View(vrange(0,1),vrange(2,3)),
         View(vrange(4,5),vrange(6,6))
@@ -33,15 +33,38 @@ object A {
     }
   }
 
-  // Compare BlitzView and List transformations, should stay consistent
-  def test1(l1: List[Int], v1: BlitzView[Int]) {
-    val v = v1 map{_ + 10} filter{_ % 2 == 0} map{_ * 1.0}
-    val l = l1 map{_ + 10} filter{_ % 2 == 0} map{_ * 1.0}
-    testFolders(l, v)
+  def testAll(l1: List[Int], v1: BlitzView[Int]) {
+    testCombOps(l1, v1)
+    testToTypes(l1, v1)
+
+    println("tests passed")
   }
 
+  // Compare BlitzView and List transformations, should stay consistent
+  def testCombOps(l1: List[Int], v1: BlitzView[Int]) {
+    val v = v1 map{_ + 10} filter{_ % 2 == 0} map{_ * 1.0}
+    val l = l1 map{_ + 10} filter{_ % 2 == 0} map{_ * 1.0}
+    testAgainstLists(l, v)
+    testConst(v)
+  }
+
+  def testToTypes(l1: List[Int], v1: BlitzView[Int])(implicit n: Numeric[Int]) {
+    def toInt(x: Int): Int = n.toInt(x)
+    def toDouble(x: Int): Double = n.toDouble(x)
+    def toFloat(x: Int): Float = n.toFloat(x)
+    def toLong(x: Int): Long = n.toLong(x)
+
+    assert(v1.toInts.toList == l1.map(toInt))
+    assert(v1.toDoubles.toList == l1.map(toDouble))
+    assert(v1.toFloats.toList == l1.map(toFloat))
+    assert(v1.toLongs.toList == l1.map(toLong))
+  }
+
+  // TODO: add tests for exceptions (eg: reduce, min, max, …)
+  // TODO: add tests for *Opt
+
   // Compare the results with different folding methods
-  def testFolders(l: List[Double], v: BlitzView[Double]) {
+  def testAgainstLists(l: List[Double], v: BlitzView[Double]) {
     assert(v.toList == l)
     assert(v.toArray.toList == l)
     assert(v.size == l.size)
@@ -66,7 +89,12 @@ object A {
 
     assert(v.sum == l.sum)
     assert(v.product == l.product)
+  }
 
-    println("tests passed")
+  // Test for static results
+  def testConst(v: BlitzView[Double]) {
+    assert(Some(v.reduce{_ + _}) == v.reduceOpt{_ + _})
+    assert(Some(v.min) == v.minOpt)
+    assert(Some(v.max) == v.maxOpt)
   }
 }
